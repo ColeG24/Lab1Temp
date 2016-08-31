c04fd1aeaf4a0f16f078283de5163147e5614e1f
coleg <coleg@lab7-25>
2016-08-31 14:36:10 -0600

    Added loop, and command to exit loop

---
+++ ConsoleApplication1/HelloWorldMainLoop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class HelloWorldMainLoop
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("Hello world");
+                String command = Console.ReadLine();
+                if ("quit".Equals(command))
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
